462d4249c51d8c802c2a9480284ecf11747b7275
Kadunapikin <ibakoshi27@gmail.com>
2025-04-24 13:58:33 +0100

    add Signup route on App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import PasswordRecovery from './pages/PasswordRecovery';
 import { Toaster } from 'react-hot-toast';
+import Signup from './pages/Signup';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/recover" element={<PasswordRecovery />} />
       </Routes>
       <Toaster position='top-right' reverseOrder={false} />
